00d90335df8f8d49b655d815199206a0c7bf2e53
svvishjs <andreww92@live.com>
2020-12-03 11:56:34 +0000

    added route to get all courses by rating

--- src/main/java/com/codeclan/example/coursebookingsystem/repositories/CourseRepository.java
+++ src/main/java/com/codeclan/example/coursebookingsystem/repositories/CourseRepository.java
@@ -3,5 +3,9 @@ package com.codeclan.example.coursebookingsystem.repositories;
 import com.codeclan.example.coursebookingsystem.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findCoursesByRating(int rating);
 }
--- src/test/java/com/codeclan/example/coursebookingsystem/CoursebookingsystemApplicationTests.java
+++ src/test/java/com/codeclan/example/coursebookingsystem/CoursebookingsystemApplicationTests.java
@@ -1,17 +1,32 @@
 package com.codeclan.example.coursebookingsystem;
 
+import com.codeclan.example.coursebookingsystem.models.Course;
+import com.codeclan.example.coursebookingsystem.repositories.CourseRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class CoursebookingsystemApplicationTests {
 
+	@Autowired
+	CourseRepository courseRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void canGetCoursesByRating() {
+		List<Course> foundCourses = courseRepository.findCoursesByRating(1);
+		assertEquals(1, foundCourses.size());
+	}
 }
